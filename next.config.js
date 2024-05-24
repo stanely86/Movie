41c1ab617ac04e0e8649219bb0249cd3599620bd
stanely86 <stanely8662@gmail.com>
2024-05-24 14:23:15 +0800

    updates done by Stanley

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 
-
 const nextConfig = {
   output:"expert",
   pageExtensions: ["js", "jsx", "ts", "tsx"],
 };
 
-module.exports = stylexPlugin({
-  rootDir: __dirname,
-  presets: ["next/babel"],
-  plugins: ["@stylexjs/babel-plugin"],
-});
 
 
 
